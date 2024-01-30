908de50f6daf38084a66b6a234fe9134549dc48a
SMHRD <SMHRD@119.200.31.90>
2024-01-30 10:16:39 +0900

    끝

--- BaseballGame/src/Model/BaseballDAO.java
+++ BaseballGame/src/Model/BaseballDAO.java
@@ -160,9 +160,9 @@ public class BaseballDAO {
 
 		try {
 			getConn();
-//			String sql = "select id, club, score" + " from (select id, club, score from baseball"
-//					+ " Order By score DESC)" + " where rownum <= 5";
-			String sql = "select id, club, score from baseball where score in(select max(score) from baseball group by id) and rownum<=10 Order By score DESC";
+			String sql = "select id, club, score" + " from (select id, club, score from baseball"
+					+ " Order By score DESC)" + " where rownum <= 10";
+//			String sql = "select id, club, score from baseball where score in(select max(score) from baseball group by id) and rownum<=10 Order By score DESC";
 
 			psmt = conn.prepareStatement(sql);
 			rs = psmt.executeQuery();
